5f8fe2cff897852a13acbbebc308f50004337253
Pradip Giri <pgiri@uplink.world>
2018-10-30 16:14:39 +0100

    Opens url inside app

--- app/src/main/java/com/example/pgiri/nsd/WebViewActivity.java
+++ app/src/main/java/com/example/pgiri/nsd/WebViewActivity.java
@@ -1,11 +1,14 @@
 package com.example.pgiri.nsd;
 
 import android.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -24,7 +27,35 @@ public class WebViewActivity extends AppCompatActivity {
         this.myWebView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        this.myWebView.setWebViewClient(new WebViewController());
         this.myWebView.loadUrl(piIP);
     }
 
+
+
+    public class WebViewController extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("https://www.example.com")) {
+                // This is my website, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
 }
+
+
